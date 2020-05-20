cec5c245bc1ad61ede119b2c2c9dcfb07c5099d1
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-05-20 23:22:51 +0300

    add ex1-2.rb

---
+++ lesson5/ex1.rb
@@ -0,0 +1,17 @@
+puts "Даров! Введи первое число"
+
+num1 = gets.chomp.to_i
+
+puts "Введи второе число"
+
+num2 = gets.chomp.to_i
+
+puts "Теперь третье число"
+
+num3 = gets.chomp.to_i
+
+average = (num1 + num2 + num3) / 3
+
+puts "Среднее арифметическое число " + average.to_s
+
+
