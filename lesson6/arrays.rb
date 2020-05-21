ffe7f709559b9e4bbdb647ba045ccc3c269d31d2
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-05-21 22:55:54 +0300

    add arr-2

---
+++ lesson6/arrays.rb
@@ -0,0 +1,14 @@
+
+fruits = ["apple", "orange", "banana"]
+
+puts "Массив: " + fruits.to_s
+
+basket = []
+
+basket << "Apple"
+
+basket.push("Mellon")
+
+basket.push("Cherry", "Mango")
+
+puts "Корзина: " + basket.to_s
