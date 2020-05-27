25673223ffb0e0347f258563604a67090bee5a70
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-05-27 23:44:14 +0300

    fin solo

--- lesson8/nightingales.rb
+++ lesson8/nightingales.rb
@@ -4,21 +4,28 @@ tem = ARGV[0]
 mon = ARGV[1]
 
 
-if Gem.win_platform? && ARGV[0]
-  tem = tem.encode('UTF-8')
+result = [
+'Соловьи поют. Иди скорее к ним!',
+'Соловьи замерзли, распеваются пока.'
+]
+
+if tem == nil
+  puts 'Какая сейчас температура?'
+  tem = STDIN.gets.chomp
 end
 
-if Gem.win_platform? && ARGV[1]
-  mon = mon.encode('UTF-8')
+if mon == nil
+  puts 'Какое время года? (0 - весна, 1 - лето, 2 - осень, 3 - зима)'
+  mon = STDIN.gets.chomp
 end
 
-if tem >= 22
-  puts 'Соловьи поют, скорее беги к ним!'
+if tem >= '22'
+  puts result[0]
 
-elsif tem >= 15 && mon == 1
-  puts 'Соловьи поют, скорее беги к ним!'
+elsif tem >= '15' && mon == '1'
+  puts result[0]
 
 else
-	puts 'Молчат, а занчит замерзли.'
+	puts result[1]
 
 end
