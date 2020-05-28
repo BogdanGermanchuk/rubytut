455cc0f6ea5ec8f17dfba91219c440216ad84691
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-05-28 23:07:15 +0300

    add parametrs.rb

---
+++ lesson9/parametrs.rb
@@ -0,0 +1,20 @@
+
+puts 'Я строка'
+
+puts 
+
+puts("Я строка 2")
+
+array = [
+  'James Bond', 'Bradd Pit', 'Dr. Who', 'House MD', 'Madonna', 'Mr. Putin'
+]
+
+puts array.to_s
+
+array.pop
+
+puts array.to_s
+
+array.pop 2
+
+puts array.to_s
