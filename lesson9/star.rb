a089f93d15f0452d0d945f1703acbaa2c1d1a021
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-05-31 23:17:23 +0300

    add star.rb

---
+++ lesson9/star.rb
@@ -0,0 +1,18 @@
+	# encoding: utf-8
+
+	def star(number)
+
+		a = '*'
+
+		number = a * number
+    
+end
+
+
+	puts 'Сколько вам звезд на погоны?'
+
+	number = gets.to_i
+
+	puts 'Вот ваши звезды :)'
+
+	puts star(number)
