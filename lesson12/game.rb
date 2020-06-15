78395049cb2c86cdf43e2d29a435aafccca24532
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-06-16 00:23:17 +0300

    vis 2.0

---
+++ lesson12/game.rb
@@ -0,0 +1,62 @@
+
+class Game
+
+	def initialize(slovo)
+		@letters = get_letters(slovo)
+
+		@errors = 0
+
+		@good_letters = []
+		@bad_letter = []
+
+		@status = 0
+	end
+
+	def get_letters(slovo)
+		if (slovo == nil || slovo == '')
+    	abort "Вы не ввели слово для игры!"
+	end
+
+		return slovo.split("")
+	end
+
+	def ask_next_letter
+		puts "\n Введите следующую букву"
+
+		letter = ""
+
+		while letter == "" do
+			letter = STDIN.gets.encode("UTF-8").chomp
+		end
+
+		next_step(letter)
+	end
+
+	def next_step(bukva)
+		if @status == -1 || @status == 1
+			return
+		end
+
+		if @good_letters.include?(bukva) || @bad_letter.include?(bukva)
+			return
+		end
+
+		if @letters.include?(bukva)
+
+			@good_letters << bukva
+
+			if @good_letters.size == @letters.uniq.size
+				@status = 1
+			end
+
+		else
+			@bad_letter << bukva
+
+			@errors += 1
+
+			if @errors >= 7
+				@status = -1
+			end
+		end
+	end
+end
