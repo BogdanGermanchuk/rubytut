918b843d1afe466215b30ac9c28a0ef59ea314fc
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-06-20 00:24:47 +0300

    viselitsa2.rb

--- lesson12/viselitsa2.rb
+++ lesson12/viselitsa2.rb
@@ -2,19 +2,13 @@
 require_relative 'game'
 require_relative 'result_printer'
 
-
+printer = ResultPrinter.new
 slovo = ARGV[0]
-  if (Gem.win_platform? && ARGV[0])
-    slovo = slovo.dup
-      .force_encoding("IBM866")
-      .encode("IBM866", "cp1251")
-      .encode("UTF-8")
-  end
 
 game = Game.new(slovo)
-printer = ResultPrinter.new
 
-while game.status == 0 do
+
+while game.status == 0
   printer.print_status(game)
   game.ask_next_letter
 end
