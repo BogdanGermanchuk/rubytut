9e814d103d263460612a495346d3bd528dd4e851
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-06-20 00:26:23 +0300

    fin

--- lesson12/game.rb
+++ lesson12/game.rb
@@ -1,83 +1,125 @@
-
+# encoding: utf-8
+#
+# Основной класс игры Game. Хранит состояние игры и предоставляет функции для
+# развития игры (ввод новых букв, подсчет кол-ва ошибок и т. п.).
+#
+# За основу взяты методы из первой версии этой игры (подробные комментарии
+# смотрите в прошлых уроках).
 class Game
+  # Конструктор — вызывается всегда при создании объекта данного класса имеет
+  # один параметр, в него нужно передавать при создании строку к загаданнмы
+  # словом. Например, game = Game.new('молоко').
+  def initialize(slovo)
+    # Инициализируем переменные экземпляра. В @letters будет лежать массив букв
+    # загаданного слова. Для того, чтобы его создать, вызываем метод get_letters
+    # этого же класса.
+    @letters = get_letters(slovo)
 
-	def initialize(slovo)
-		@letters = get_letters(slovo)
+    # Переменная @errors будет хранить текущее количество ошибок, всего можно
+    # сделать не более 7 ошибок. Начальное значение — 0.
+    @errors = 0
 
-		@errors = 0
+    # Переменные @good_letters и @bad_lettes будут содержать массивы, хранящие
+    # угаданные и неугаданные буквы. В начале игры они пустые.
+    @good_letters = []
+    @bad_letters = []
 
-		@good_letters = []
-		@bad_letters = []
+    # Специальная переменная-индикатор состояния игры (см. метод get_status)
+    @status = 0
+  end
 
-		@status = 0
-	end
+  # Метод, который возвращает массив букв загаданного слова
+  def get_letters(slovo)
+    if slovo == nil || slovo == ''
+      abort 'Для игры введите загаданное слово в качестве аргумента при ' \
+        'запуске программы'
+    end
 
-	def get_letters(slovo)
-		if slovo == nil || slovo == ''
-    	abort "Вы не ввели слово для игры!"
-	end
+    return slovo.encode('UTF-8').split('')
+  end
 
-		return slovo.split("")
-	end
+  # Метод, возвращающий статус игры (геттер для @status)
+  #
+  #  0 – игра активна
+  # -1 – игра закончена поражением
+  #  1 – игра закончена победой
+  def status
+    return @status
+  end
 
-	def status
-  	return @status
-	end
+  # Основной метод игры "сделать следующий шаг". В качестве параметра принимает
+  # букву, которую ввел пользователь. Основная логика взята из метода
+  # check_user_input (см. первую версию программы).
+  def next_step(bukva)
+    # Предварительная проверка: если статус игры равен 1 или -1, значит игра
+    # закончена и нет смысла дальше делать шаг. Выходим из метода возвращая
+    # пустое значение.
+    if @status == -1 || @status == 1
+      return
+    end
 
-	def next_step(bukva)
-		if @status == -1 || @status == 1
-			return
-		end
+    # Если введенная буква уже есть в списке "правильных" или "ошибочных" букв,
+    # то ничего не изменилось, выходим из метода.
+    if @good_letters.include?(bukva) || @bad_letters.include?(bukva)
+      return
+    end
 
-		if @good_letters.include?(bukva) || @bad_letters.include?(bukva)
-			return
-		end
+    if @letters.include?(bukva)
+      # Если в слове есть буква запишем её в число "правильных" буква
+      @good_letters << bukva
 
-		if @letters.include?(bukva)
+      # Дополнительная проверка — угадано ли на этой букве все слово целиком.
+      # Если да — меняем значение переменной @status на 1 — победа.
+      if @good_letters.uniq.sort == @letters.uniq.sort
+        @status = 1
+      end
+    else
+      # Если в слове нет введенной буквы — добавляем эту букву в массив
+      # «плохих» букв и увеличиваем счетчик ошибок.
+      @bad_letters << bukva
+      @errors += 1
 
-			@good_letters << bukva
+      # Если ошибок больше 7 — статус игры меняем на -1, проигрыш.
+      if @errors >= 7
+        @status = -1
+      end
+    end
+  end
 
-			if @good_letters.uniq.sort == @letters.uniq.sort
-				@status = 1
-			end
+  # Метод, спрашивающий юзера букву и возвращающий ее как результат. В идеале
+  # этот метод лучше вынести в другой класс, потому что он относится не только
+  # к состоянию игры, но и к вводу данных.
+  def ask_next_letter
+    puts "\nВведите следующую букву"
 
-		else
-			@bad_letters << bukva
-			@errors += 1
+    letter = ''
+    while letter == ''
+      letter = STDIN.gets.encode('UTF-8').chomp
+    end
 
-			if @errors >= 7
-				@status = -1
-			end
-		end
-	end
+    # После получения ввода, передаем управление в основной метод игры
+    next_step(letter)
+  end
 
-	def ask_next_letter
-		puts "\n Введите следующую букву"
+  # Методы, так называемые accessors или геттеры. Смысл каждого метода — только
+  # предоставить доступ к внутренним переменным экземпляра класса. Без таких
+  # методов, например к @letters, @errors нет доступа ни у кого, кроме самого
+  # объекта данного класса
+  def errors
+    # Важная фишка Ruby: слово return можно опустить, если это последняя строка
+    # в методе. Последнее вычисленное значение в методе и будет возвращено.
+    @errors
+  end
 
-	letter = ""
+  def letters
+    @letters
+  end
 
-	while letter == ""
-		letter = STDIN.gets.encode("UTF-8").chomp
-	end
+  def good_letters
+    @good_letters
+  end
 
-	next_step(letter)
-end
-
-	
-
-def errors
-  return @errors
-end
-
-def letters
-  return @letters
-end
-
-def good_letters
-  return @good_letters
-end
-
-def bad_letters
-  return @bad_letters
-end
+  def bad_letters
+    @bad_letters
+  end
 end
