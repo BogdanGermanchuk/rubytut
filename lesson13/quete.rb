5ce81118467c1807271464a1fa91881b53b13467
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-06-23 00:19:06 +0300

    quete.rb

---
+++ lesson13/quete.rb
@@ -0,0 +1,15 @@
+
+current_path = File.dirname(__FILE__)
+
+file_path = current_path + '/data/afor.txt'
+
+puts "Афоризм дня:"
+
+if File.exist?(file_path)
+	f = File.new(file_path, 'r:UTF-8')
+	lines = f.readlines
+	f.close
+	puts lines.sample
+else
+	puts 'Файл не найден'
+end
